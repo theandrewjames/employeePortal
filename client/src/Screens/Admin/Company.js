35ba988720e69616c595883fee3679bf83389a4f
Mei-ziu <dylan02nguyen@gmail.com>
2023-04-24 12:15:30 -0700

    Created temporary dropdown menu

--- client/src/Screens/Admin/Company.js
+++ client/src/Screens/Admin/Company.js
@@ -1,21 +1,46 @@
 import { Navigate } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 import { userState } from '../../globalstate'
+import { useState } from 'react'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
 const CompanyScreen = () => {
-    const [user, setUser] = useRecoilState(userState)
+  const [user, setUser] = useRecoilState(userState)
 
-    if (!user.isLoggedIn) {
-        return <Navigate replace to="/" />
-    } else if (!user.isAdmin) {
-        return <Navigate replace to="/announcements" />
-    }
-    else {
-        return (
-            <div>
-                <h1>Company</h1>
-            </div>
-        )
-    }
+  //Get companies that admin can access
+  const [company, setCompany] = useState('')
+
+  const handleChange = (event) => {
+    setCompany(event.target.value)
+  }
+
+  if (!user.isLoggedIn) {
+    return <Navigate replace to='/' />
+  } else if (!user.isAdmin) {
+    return <Navigate replace to='/announcements' />
+  } else {
+    return (
+      <div>
+        <h1>Select Company</h1>
+        <FormControl fullWidth>
+          <InputLabel>Pick a company</InputLabel>
+          <Select
+            value={company}
+            label='Pick a company'
+            onChange={handleChange}
+          >
+            {user.companies.map((company) => (
+              <MenuItem key={company} value={company}>
+                {company}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    )
+  }
 }
 
 export default CompanyScreen
