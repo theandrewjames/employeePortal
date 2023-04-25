03fb56118c50224d1280f846faf4c833db6fb1b0
Shilpa Nair <101418016+lafleurss@users.noreply.github.com>
2023-04-25 14:59:50 -0500

    wip

--- client/src/Services/announcements.js
+++ client/src/Services/announcements.js
@@ -6,6 +6,7 @@ export const getAnnouncements = async (id) => {
 }
 
 export const saveAnnouncement = async (id, title, message, user) => {
+  console.log("*********")
   console.log(user)
   console.log(title)
   console.log(message)
@@ -17,22 +18,3 @@ export const saveAnnouncement = async (id, title, message, user) => {
   })
   return response.data
 }
-
-// const response = await api.post("/users/login", {
-//   username: username,
-//   password: password,
-// });
-
-// BasicUserDto
-// id: long,
-// profile: ProfileDto,
-// isAdmin: boolean,
-// active: boolean,
-// status: string
-
-// { ProfileDto
-//   firstname: string,
-//   lastname: string,
-//   email:string,
-//   phone: string
-// }
