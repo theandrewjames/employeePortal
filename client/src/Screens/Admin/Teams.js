0e35642ff41a9b89b47217192706452ce2382171
busma13 <peterluitjens@gmail.com>
2023-04-25 17:48:07 -0700

    Add clickHandler from team to their projects.

--- client/src/Screens/Admin/Teams.js
+++ client/src/Screens/Admin/Teams.js
@@ -1,10 +1,10 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, redirect } from "react-router-dom"
 import { useRecoilState } from "recoil"
 import NavBar from "../../Components/NavBar"
-import { userState } from "../../globalstate"
-import { companyState } from "../../globalstate"
+import { userState, companyState, currentTeamState } from "../../globalstate"
 import { Button, Card, CardContent, CardHeader } from "@mui/material"
 import CreateTeamOverlay from "../../Components/CreateTeamOverlay"
+import { useEffect, useState } from "react"
 
 const handleCreateNewTeam = () => {
   console.log("Add new team")
@@ -13,93 +13,53 @@ const handleCreateNewTeam = () => {
 const Teams = () => {
   const [user, setUser] = useRecoilState(userState)
   const [company, setCompany] = useRecoilState(companyState)
+  const [currentTeam, setCurrentTeam] = useRecoilState(currentTeamState)
+  const [teamSelected, setTeamSelected] = useState(false)
 
-  const tempCompany = {
-    id: 1,
-    name: "Company 1",
-    description: "desc",
-    teams: [
-      {
-        id: 2,
-        name: "team1",
-        description: "team1 desc",
-        users: [
-          {
-            id: 4,
-            profile: {
-              firstname: "Peter",
-              lastname: "Luitjens",
-            },
-          },
-          {
-            id: 3,
-            profile: {
-              firstname: "Shilpa",
-              lastname: "Nair",
-            },
-          },
-          {
-            id: 1,
-            profile: {
-              firstname: "Dylan",
-              lastname: "Nguyen",
-            },
-          },
-          {
-            id: 2,
-            profile: {
-              firstname: "Jiwon",
-              lastname: "Shim",
-            },
-          },
-        ],
-      },
-      {
-        id: 3,
-        name: "team2",
-        description: "team2 desc",
-        users: [
-          {
-            id: 1,
-            profile: {
-              firstname: "Dylan",
-              lastname: "Nguyen",
-            },
-          },
-          {
-            id: 2,
-            profile: {
-              firstname: "Jiwon",
-              lastname: "Shim",
-            },
-          },
-        ],
-      },
-    ],
-    users: [],
+  useEffect(() => {
+    console.log(company)
+    console.log(currentTeam)
+  }, [company, currentTeam, setCurrentTeam])
+
+  const handleCardClick = (event) => {
+    console.log(event.currentTarget.dataset.id)
+    const team = company[0].teams.filter(
+        (team) => team.id == event.currentTarget.dataset.id
+      )[0]
+    console.log(team)
+
+    setCurrentTeam(
+      team
+    )
+    setTeamSelected(true)
   }
 
   if (!user.isLoggedIn) {
     return <Navigate replace to='/' />
   } else {
-    return (
-      <div style={{ height: '100%',
-        width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', }}>
+    return teamSelected ? (
+      <Navigate replace to='/projects' />
+    ) : (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
         <NavBar />
         <h1 style={{ marginTop: "6vh" }}>Teams</h1>
-        {tempCompany.teams.map((team) => (
-          <Card key={team.id}>
+        {company[0]?.teams?.map((team) => (
+          <Card key={team.id} data-id={team.id} onClick={handleCardClick}>
             <CardHeader title={team.name} subheader='# of Projects: 5' />
             <CardContent>
               <h3>Members</h3>
-              {team.users.map((user) => (
-                <Button
-                  key={user.id}
-                  variant='contained'
-                  href=''
-                  onClick={() => console.log("User clicked")}
-                >
-                  {user.profile.firstname} {user.profile.lastname.slice(0, 1)}.
+              {team?.teammates?.map((user) => (
+                <Button key={user.id} variant='contained'>
+                  {user?.profile?.firstName}{" "}
+                  {user?.profile?.lastName?.slice(0, 1)}.
                 </Button>
               ))}
             </CardContent>
