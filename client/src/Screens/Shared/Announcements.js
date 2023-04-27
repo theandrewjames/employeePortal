d3863ac40ba937ce14152a240747d2b5faf6f4f3
Shilpa Nair <101418016+lafleurss@users.noreply.github.com>
2023-04-27 10:46:10 -0500

    ui

--- client/src/Screens/Shared/Announcements.js
+++ client/src/Screens/Shared/Announcements.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
 import NavBar from "../../Components/NavBar"
-import { userState, companyState } from "../../globalstate"
+import { userState, companyState, errorState } from "../../globalstate"
 import { Button, Box, TextField } from "@mui/material"
 import {
   Dialog,
@@ -24,6 +24,7 @@ const Announcements = () => {
   const [openNewDialog, setOpenNewDialog] = useState(false)
   const [title, setTitle] = useState("")
   const [message, setMessage] = useState("")
+  const [formError, setFormError] = useRecoilState(errorState)
 
   const btnstyle = {
     margin: "2px 0",
@@ -78,6 +79,73 @@ const Announcements = () => {
     display: flex;
     justify-content: center;
   `
+
+  const formStyle = {
+    background: "#051622",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    padding: "0 50px",
+    height: "100%",
+    textAlign: "center",
+    color: "#deb992",
+  }
+
+  const Title = {
+    fontWeight: "bold",
+    margin: 0,
+    fontSize: "25px",
+    padding: "10px",
+    marginBottom: "15px",
+  }
+
+  const textfieldStyle = {
+    backgroundColor: "transparent",
+    border: "none",
+    borderBottom: "2px solid #deb992",
+    padding: "7px 15px",
+    margin: "5px 0",
+    width: "90%",
+    color: "#deb992",
+    "&::placeholder": {
+      color: "#deb992",
+    },
+    "&:focus": {
+      outline: "none",
+    },
+  }
+
+  const Button = styled.button`
+    background: #1ba098;
+    border-radius: 10.2875px;
+    border: 1px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    padding: 12px 45px;
+    font-weight: bold;
+    cursor: pointer;
+    color: rgba(255, 255, 255, 0.75);
+    margin-top: 25px;
+  `
+
+  const StyledModal = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    transition: "transform 0.6s ease-in-out",
+    textAlign: "center",
+    zIndex: 100,
+    width: "50%",
+    height: "70%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    padding: "0 50px",
+  }
+
   useEffect(() => {
     async function fetchData() {
       const data = await getAnnouncements(user.companies[0].id)
@@ -90,12 +158,26 @@ const Announcements = () => {
     setOpenNewDialog(true)
   }
 
+  const formIsValid = () => {
+    if (!title || !message) {
+      setFormError({
+        ...formError,
+        isError: true,
+        message: "All fields are required",
+      })
+      return false
+    }
+    return true
+  }
+
   const saveAnnoucement = async () => {
     setOpenNewDialog(false)
     console.log(user)
-    const savedData = await saveAnnouncement(company.id, title, message, user)
-    console.log("Saving announcements...")
-    setAnnouncementUpdate(true)
+    if (formIsValid()) {
+      const savedData = await saveAnnouncement(company.id, title, message, user)
+      console.log("Saving announcements...")
+      setAnnouncementUpdate(true)
+    }
   }
 
   if (!user.isLoggedIn) {
@@ -169,15 +251,21 @@ const Announcements = () => {
               </GridItem>
             ))}
           </GridContainer>
-          <Dialog open={openNewDialog} onClose={() => setOpenNewDialog(false)}>
-            <DialogTitle>Create new announcement</DialogTitle>
+          <Dialog
+            style={StyledModal}
+            open={openNewDialog}
+            onClose={() => setOpenNewDialog(false)}
+          >
+            <DialogTitle style={Title}>Create new announcement</DialogTitle>
             <TextField
+              style={textfieldStyle}
               label="Title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               fullWidth
             />
             <TextField
+              style={textfieldStyle}
               label="Message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
@@ -186,6 +274,29 @@ const Announcements = () => {
             <DialogActions>
               <Button onClick={() => saveAnnoucement()}>Submit</Button>
             </DialogActions>
+            {formError.isError && !formError.field ? (
+              <p
+                style={{
+                  color: "red",
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                {formError.message}
+              </p>
+            ) : formError.isError && formError.field ? (
+              <p
+                style={{
+                  color: "red",
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                {formError.message}
+              </p>
+            ) : (
+              ""
+            )}
           </Dialog>
         </div>
       </>
